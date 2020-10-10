accba1a6afd856b05bd3468d5d470e1141adbae6
Jordan Ryan <jrm4a2@gmail.com>
2020-10-10 11:46:16 -0600

    Removed usless loop

--- Lab-1/top_level.vhd
+++ Lab-1/top_level.vhd
@@ -5,7 +5,7 @@ use ieee.numeric_std.all;
 entity top_level is
     Port ( clk                           : in  STD_LOGIC;
            reset_n                       : in  STD_LOGIC;
-			  SW                            : in  STD_LOGIC_VECTOR (9 downto 0);
+           SW                            : in  STD_LOGIC_VECTOR (9 downto 0);
            LEDR                          : out STD_LOGIC_VECTOR (9 downto 0);
            HEX0,HEX1,HEX2,HEX3,HEX4,HEX5 : out STD_LOGIC_VECTOR (7 downto 0)
           );
@@ -18,9 +18,11 @@ Signal Num_Hex0, Num_Hex1, Num_Hex2, Num_Hex3, Num_Hex4, Num_Hex5 : STD_LOGIC_VE
 Signal DP_in, Blank:  STD_LOGIC_VECTOR (5 downto 0);
 Signal switch_inputs: STD_LOGIC_VECTOR (12 downto 0);
 Signal bcd:           STD_LOGIC_VECTOR(15 DOWNTO 0);
-signal d0: STD_LOGIC_VECTOR(15 downto 0);
-signal d1: STD_LOGIC_VECTOR(15 downto 0);
-signal y: STD_LOGIC_VECTOR(15 downto 0);
+signal d0:            STD_LOGIC_VECTOR(15 downto 0);
+signal d1:            STD_LOGIC_VECTOR(15 downto 0);
+signal y:             STD_LOGIC_VECTOR(15 downto 0);
+
+signal test: STD_LOGIC := '0';
 
 component mux2 is
 	Port(
@@ -46,6 +48,7 @@ Component binary_bcd IS
 END Component;
 
 begin
+
    Num_Hex0 <= y(3  downto  0); 
    Num_Hex1 <= y(7  downto  4);
    Num_Hex2 <= y(11 downto  8);
