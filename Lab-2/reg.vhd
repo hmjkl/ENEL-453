852de5372a02658c3cb12e944d06dff7983cae8e
hmjkl <jrm4a1@gmail.com>
2020-10-19 15:10:38 -0600

    changed to async reset

--- Lab-2/reg.vhd
+++ Lab-2/reg.vhd
@@ -1,21 +1,24 @@
 library IEEE; use IEEE.STD_LOGIC_1164.ALL;
 
-entity DFF_C is
+entity reg is
   generic(sz : integer := 1);
   port(D : in STD_LOGIC_VECTOR(sz - 1 downto 0);
        We : in STD_LOGIC;
        clk : in STD_LOGIC;
+       reset_n : in STD_LOGIC;
        Q : out STD_LOGIC_VECTOR(sz - 1 downto 0));
 end;
 
-architecture Behavioral of DFF_C is
+architecture Behavioral of reg is
 begin
 
-  process(clk) begin
-    if (rising_edge(clk)) then
-	   if (We = '1') then
+  process(clk, reset_n) begin
+    if (reset_n = '0') then
+      Q <= (others => '0');
+    elsif (rising_edge(clk)) then
+      if (We = '1') then
         Q <= D;
-		end if;
+      end if;
     end if;
   end process;
 
