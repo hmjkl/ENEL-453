d4901f30830cca406d24b276728e5c5e8e4c46f5
Jordan Ryan <jrm4a2@gmail.com>
2020-12-05 22:17:02 -0700

    changed size of counter to make blinking faster

--- Lab-4/seven_seg_controller.vhd
+++ Lab-4/seven_seg_controller.vhd
@@ -44,7 +44,9 @@ architecture Behavioral of seven_seg_controller is
   end component;
 
   signal PWM_EN            : std_logic;
-  signal seven_seg_max_cnt : integer range 0 to 8191;
+  -- The size of seven_seg_max_cnt is given by seven_seg_max_cnt(8191), which
+  -- is determined below.
+  signal seven_seg_max_cnt : integer range 0 to 2047;
   signal blank_val_tmp     : std_logic;
 
 begin
@@ -52,24 +54,28 @@ begin
   process(clk)
   begin
     if rising_edge(clk) then
+      -- This is a mostly arbitary function. It was chosen for being easy to
+      -- make while providing a wide range of speeds for blinking.
       seven_seg_max_cnt <= to_integer((distance srl 2) - 10);
     end if;
   end process;
 
   i_counter_1 : counter
-    generic map(CLK_DIV_SCALE => 8191)
+    generic map(CLK_DIV_SCALE => 2047)
     port map(clk     => clk,
              reset_n => reset_n,
              max_cnt => seven_seg_max_cnt,
              en      => '1',
              Y       => PWM_EN);
 
+  -- The size of the PWM is picked somewhat arbitarly, But all that is needed
+  -- is a semi-large fund. freq.
   i_PWM_1 : PWM
-    generic map(sz => 17)
+    generic map(sz => 16)
     port map(clk        => clk,
              reset_n    => reset_n,
              en         => PWM_EN,
-             duty_cycle => "0" & X"FFFF",
+             duty_cycle => X"8000",
              y          => open,
              y_inv      => blank_val_tmp);
 
